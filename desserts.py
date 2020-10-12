9d7365b35704485f93e5ab44ab6dce84fde489a4
vagrant <vagrant@vagrant.vm>
2020-10-12 20:03:22 +0000

    Added stock and sell methods

--- desserts.py
+++ desserts.py
@@ -3,11 +3,35 @@
 
 class Cupcake:
     """A cupcake."""
+    cache = {}
+    
+    def __init__(self, name, flavor, price, qty=0):
+        """Makes a Cupcake instance"""
+        self.name = name
+        self.flavor = flavor
+        self.price = price
+        self.qty = qty
+        self.cache[name] = [flavor, price, qty]
+    
+    def add_stock(self, amount):
+        self.qty = amount + self.qty
 
-    def __repr__(self):
-        """Human-readable printout for debugging."""
+    def sell(self, amount):
+        """Sell and update cupcake qty"""
 
-        return f'<Cupcake name="{self.name}" qty={self.qty}>'
+        if self.qty == 0:
+            print("Sorry, these cupcakes are sold out!")
+        
+        elif self.qty - amount < 0:
+            print(f"Sorry there are only {qty} cupcakes available.")
+
+        else:
+            self.qty = self.qty - amount
+    
+    # def __repr__(self):
+    #     """Human-readable printout for debugging."""
+
+    #     return f'<Cupcake name="{self.name}" qty={self.qty}>'
 
 
 if __name__ == '__main__':
